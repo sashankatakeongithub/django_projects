c16571c83ce2598597f65de73058d45a81c84a31
sashankatakeongithub <shanky7612@gmail.com>
2019-10-23 19:00:20 +0000

    After Skeleton

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6=cmwvmyvh4nj0k3+9)opjwfc=qd!)6bzuq)zffwt*aonhl7cn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# Use include() to add paths from the catalog application
+from django.urls import include
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/')),
+    ]
